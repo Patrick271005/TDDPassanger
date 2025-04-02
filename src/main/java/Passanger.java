1ad74f90dfdd5a4b9d0668610d3eb1ff062356a2
Patrick Flanagan <G00431255@atu.ie>
2025-04-02 16:47:15 +0100

    Passed Inital test phase

---
+++ src/main/java/Passanger.java
@@ -0,0 +1,34 @@
+public class Passanger {
+    private String title;
+    private String firstName;
+    private String lastName;
+    public Passanger(String title, String firstName, String lastName) {
+        this.title = title;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
